d9165957e09a0d2014d0ade3800c946c1067028f
Jose Mesa <josemesapadilla@gmail.com>
2025-07-06 13:37:05 +0200

    Actualizar next.config.js para corregir assetPrefix y basePath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
+  output: 'export',
+  assetPrefix: '/',  // Correcto para sitio en la raíz
+  basePath: '',      // Asegúrate que basePath esté vacío para evitar prefijos extra
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../'),
   },
@@ -13,7 +15,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
